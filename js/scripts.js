9be0856a0e5091b86af0cf1a8d3c4cbe847b46f5
Priyanka-BN11 <priyankabnpinky@gmail.com>
2022-07-01 15:18:01 +0530

    Updated 4 Pokemons

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,25 @@
-alert('Hello world');
-let favoriteFood = 'Pizza'
-document.write(favoriteFood);
+let pokemonList =[
+    {
+        name: 'Bulbasaur',
+        height: 0.7,
+        weight: 6.9,
+        types: ['grass', 'poison']
+    },
+    {
+        name: 'Onix',
+        height:8.8,
+        weight: 210,
+        types: ['rock', 'ground']
+    },{
+        name:'Togepi',
+        height:0.3,
+        weight:1.5,
+        types: ['fairy']
+    },
+    {
+        name:'Pikachu',
+        height: 0.4,
+        weight: 6,
+        types: ['electric']
+    }
+];
